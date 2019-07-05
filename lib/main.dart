e8c9aac4409ffc68e404009ca6d9718f7ebdea65
f-person <agakaryan.arshak@gmail.com>
2019-07-05 12:57:45 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MealsApp());
+}
+
+class MealsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals',
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+      ),
+      body: Center(
+        child: Text('Flutter project'),
+      ),
+    );
+  }
+}
