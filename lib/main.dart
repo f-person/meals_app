f008d42d51c2848b23320fbca3158930d3a0b41e
f-person <agakaryan.arshak@gmail.com>
2019-07-07 21:49:16 +0400

    Add BottomNavigationTabBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
 
 void main() {
   runApp(MealsApp());
@@ -41,7 +42,7 @@ class MealsApp extends StatelessWidget {
           builder: (BuildContext context) => CategoriesScreen(),
         );
       },
-      home: CategoriesScreen(),
+      home: TabsScreen(),
     );
   }
 }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,23 +6,20 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meals'),
+    return GridView(
+      padding: const EdgeInsets.all(20.0),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200.0,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20.0,
+        mainAxisSpacing: 20.0,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(20.0),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200.0,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20.0,
-          mainAxisSpacing: 20.0,
-        ),
-        children: DUMMY_CATEGORIES
-            .map((categoryData) => CategoryItem(
-                categoryData.id, categoryData.title, categoryData.color))
-            .toList(),
-      ),
+      children: DUMMY_CATEGORIES
+          .map(
+            (categoryData) => CategoryItem(
+                categoryData.id, categoryData.title, categoryData.color),
+          )
+          .toList(),
     );
   }
 }
